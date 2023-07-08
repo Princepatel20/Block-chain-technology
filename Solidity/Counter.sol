79f183c958f832465f885edf5d701d58acf0be78
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-07-08 19:11:32 +0530

    Create Counter.sol

---
+++ Solidity/Counter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Counter {
+    uint public count = 0;
+    
+    function getCount() public view returns (uint) {
+        return count;
+    }
+
+    function incrementCount() public {
+        count++;
+    }
+}
+
